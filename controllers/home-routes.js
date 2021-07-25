c9ec31a95b7cc4621f5566610065d07cc38274db
jerry <jerry4905@hotmail.com>
2021-07-24 18:32:08 -0700

    logout added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,27 +3,23 @@ const sequelize = require('../config/connection');
 const { Contactus } = require('../models');
 
 router.get('/', (req, res) => {
-        res.render('contact-us', { Contactus });
-      })
-      // .catch(err => {
-      //   console.log(err);
-      //   res.status(500).json(err);
-      // });
-  // });
+  console.log(req.session);
+  res.render('contact-us', { Contactus });
+})
+// .catch(err => {
+//   console.log(err);
+//   res.status(500).json(err);
+// });
+// });
 
-  // router.get('/login', (req, res) => {
-  //   res.render('login');
-  // });
-  
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
-
-// const router = require('express').Router();
-// const sequelize = require('../config/connection');
-// const { User } = require('../models');
-
-// router.get('/login', (req, res) => {
-//     res.render('login');
-//   });
-
-// module.exports = router;
 
