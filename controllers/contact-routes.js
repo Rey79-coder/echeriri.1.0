a009d1c21fa4d341bd868f19517a2405b95d939c
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-24 19:30:00 -0700

    team/handlebar

--- controllers/contact-routes.js
+++ controllers/contact-routes.js
@@ -3,23 +3,44 @@ const router = require('express').Router();
 const { Contact } = require('../models');
 const { router } = require('./home-routes');
 
- 
-router.get('/', (req, res) => {
 
-namespace.Contact.Controllers; 
-{ 
-    public.class.ContactController; Controller 
-    { 
-        // 
-        // GET: /HelloWorld/ 
- 
-        public.url.Index() 
-        { 
-            return ('./views/main.handlebars'); 
-        } 
-    } 
-  }
+
+// const express = require('express')
+// const server = express()
+
+/* route requests for static files to appropriate directory */
+router.use('/public', express.static(__dirname + './views/contact.handlebars'))
+
+/* other routes defined before catch-all */
+router.get('/contact', (req, res) => {
+  res.send('ok')
 })
 
-app.use(express.static(__dirname + '/public'));
+/* final catch-all route to index.html defined last */
+router.get('/*', (req, res) => {
+  res.sendFile(__dirname + './views/layouts/main.handlebars');
+})
+
+module.exports = router;
+
+
+
+      // THIS IS WORKING SOMEHOW
+// router.get('/', (req, res) => {
+
+// namespace.Contact.Controllers; 
+// { 
+//     public.class.ContactController; Controller 
+//     { 
+ 
+//         public.url.Index() 
+        
+//         { 
+//             return ('./views/contact.handlebars'); 
+//         } 
+//     } 
+//   }
+// })
+
+// app.use(express.static(__dirname + '/public'));
 
--- models/Team.js
+++ models/Team.js
@@ -1,23 +1,23 @@
 const sequelize = require('../config/connection');
-const stylesheet = require('./css/style.css')
+const stylesheet = require('public/css/team.css')
 
 myStorage = window.localStorage;
 
 // STICKY HEADER / When the user scrolls the page, execute myFunction
-window.onscroll = function() {Contact()};
+window.onscroll = function() {Team()};
 
 // Get the header
-var Contact = document.getElementById("contact-us");
+var Team = document.getElementById("about");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function Contact() {
+function Team() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
 }}
 
 app.use(express.static(__dirname + '/public'));
 
-module.exports = Contact;
+module.exports = Team;
