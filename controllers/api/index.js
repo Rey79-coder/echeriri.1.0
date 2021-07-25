1d3cb5937c3b05135833afd9cf67b40cd441b045
genesissiana <perezgenesisa@gmail.com>
2021-07-25 16:17:11 -0700

    routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
-
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
+const fireRoutes = require('./fire-routes');
+
 router.use('/users', userRoutes);
 
-module.exports = router;
+
+module.exports = router;  
--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const sess = {
 
 app.use(session(sess));
 
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -35,6 +36,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 
 // turn on routes
