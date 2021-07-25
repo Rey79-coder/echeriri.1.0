f48679c7b112e19ebf8e6b5fd53d624a67aacb35
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-25 10:43:21 -0700

    3homepage

--- controllers/contact-routes.js
+++ controllers/contact-routes.js
@@ -5,42 +5,43 @@ const { router } = require('./home-routes');
 
 
 
-// const express = require('express')
-// const server = express()
+// // const express = require('express')
+// // const server = express()
 
-/* route requests for static files to appropriate directory */
-router.use('/public', express.static(__dirname + './views/contact.handlebars'))
+// /* route requests for static files to appropriate directory */
+// router.use('/public', express.static(__dirname + './views/contact.handlebars'))
 
-/* other routes defined before catch-all */
-router.get('/contact', (req, res) => {
-  res.send('ok')
-})
+// /* other routes defined before catch-all */
+// router.get('/contact', (req, res) => {
+//   res.send('ok')
+// })
 
-/* final catch-all route to index.html defined last */
-router.get('/*', (req, res) => {
-  res.sendFile(__dirname + './views/layouts/main.handlebars');
-})
+// /* final catch-all route to index.html defined last */
+// router.get('/*', (req, res) => {
+//   res.sendFile(__dirname + './views/layouts/main.handlebars');
+// })
 
-module.exports = router;
+// module.exports = router;
 
 
 
       // THIS IS WORKING SOMEHOW
-// router.get('/', (req, res) => {
+router.get('/', (req, res) => {
 
-// namespace.Contact.Controllers; 
-// { 
-//     public.class.ContactController; Controller 
-//     { 
+namespace.Contact.Controllers; 
+{ 
+    public.class.ContactController; Controller 
+    { 
  
-//         public.url.Index() 
+        public.url.Index() 
         
-//         { 
-//             return ('./views/contact.handlebars'); 
-//         } 
-//     } 
-//   }
-// })
+        { 
+            return ('./views/main.handlebars'); 
+        } 
+    } 
+  }
+})
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
+module.exports = router;
