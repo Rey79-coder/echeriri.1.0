ebb90f28070c115ee375613ea062f98f500eccd7
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-25 20:57:56 -0700

    homepage-done

--- server.js
+++ server.js
@@ -28,9 +28,11 @@ const sess = {
 
 var cookieParser = require('cookie-parser')
  
+
+// app.use(express.static(path.join(__dirname, '/public')));
+
 app.use(cookieParser());
 app.use(session(sess));
-
 
 
 app.engine('handlebars', hbs.engine);
@@ -42,6 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
 // turn on routes
 app.use(routes);
 
@@ -50,55 +53,3 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => 
   console.log(`Now listening on port ${PORT}!`));
 });
-
-
-// const helpers = require('./utils/helpers');
-// const express = require('express');
-// // const routes = require('./routes');
-// const sequelize = require('./config/connection');
-// const path = require('path');
-// const routes = require('./controllers/');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({ helpers });
-
-// const session = require('express-session');
-
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const sess = {
-//   secret: process.env.Secret,
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// const dotenv = require('dotenv').config()
-
-// app.use(express.cookieParser('your secret here'));
-// app.use(express.session());
-// app.use(session(sess));
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
-// // turn on routes
-// app.use(routes);
-
-// // turn on connection to db and server
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => 
-//   console.log(`Now listening on port ${PORT}!`));
-// });
